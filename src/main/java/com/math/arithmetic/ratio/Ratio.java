795bec0df806ed68896467f9639f269e45cf240c
Arkadiy Seletskiy <arkadius2006@gmail.com>
2017-07-02 21:09:31 +0300

    AR-3: ratio draft

---
+++ src/main/java/com/math/arithmetic/ratio/Ratio.java
@@ -0,0 +1,9 @@
+package com.math.arithmetic.ratio;
+
+/**
+ * Created by arkadiy on 02/07/17.
+ */
+public interface Ratio {
+
+    RatioArithmetic getRatioArithmetic();
+}
---
+++ src/main/java/com/math/arithmetic/ratio/RatioArithmetic.java
@@ -0,0 +1,8 @@
+package com.math.arithmetic.ratio;
+
+/**
+ * Created by arkadiy on 02/07/17.
+ */
+public interface RatioArithmetic {
+    // todo provide basic operations and relations
+}
