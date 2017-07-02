3736df12b5330fd7f2e3a0e701112ba69c43b2ad
Arkadiy Seletskiy <arkadius2006@gmail.com>
2017-07-02 13:48:27 +0300

    AR-1: natural and natural arithmetic interfaces

---
+++ src/main/java/com/math/arithmetic/natural/Natural.java
@@ -0,0 +1,9 @@
+package com.math.arithmetic.natural;
+
+/**
+ * Created by arkadiy on 02/07/17.
+ */
+public interface Natural {
+
+    NaturalArithmetic getNaturalArithmetic();
+}
---
+++ src/main/java/com/math/arithmetic/natural/NaturalArithmetic.java
@@ -0,0 +1,64 @@
+package com.math.arithmetic.natural;
+
+import com.math.BinaryRelation;
+import com.math.Relation;
+
+import java.util.function.BinaryOperator;
+
+/**
+ * Created by arkadiy on 02/07/17.
+ */
+public interface NaturalArithmetic {
+
+    // relations and operations
+
+    Relation<Natural> isZero();
+
+    BinaryRelation<Natural> isLessThan();
+
+    BinaryRelation<Natural> isEqualTo();
+
+    BinaryRelation<Natural> isGreaterThan();
+
+    BinaryRelation<Natural> isDivisorOf();
+
+    BinaryRelation<Natural> isMultipleOf();
+
+    BinaryOperator<Natural> addition();
+
+    BinaryOperator<Natural> multiplication();
+
+    BinaryOperator<Natural> quotient();
+
+    BinaryOperator<Natural> remainder();
+
+
+    // factory methods
+
+    Natural of(byte value);
+
+    Natural of(int value);
+
+    Natural of(long value);
+
+    Natural ofDecimal(String value) throws NumberFormatException;
+
+    Natural ofBinary(String value) throws NumberFormatException;
+
+    Natural ofHexidecimal(String value) throws NumberFormatException;
+
+
+    // convertion back to java primitives methods
+
+    byte toByte(Natural value) throws ArithmeticException;
+
+    int toInt(Natural value) throws ArithmeticException;
+
+    long toLong(Natural value) throws ArithmeticException;
+
+    String toDecimal(Natural value);
+
+    String toBinary(Natural value);
+
+    String toHexidecimal(Natural value);
+}
